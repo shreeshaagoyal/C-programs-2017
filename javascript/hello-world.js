8a64059e49de88b9acbc506c3402c37c66569a4c
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-05-28 12:15:53 -0700

    array index program

--- javascript/hello-world.js
+++ javascript/hello-world.js
@@ -1,8 +1,10 @@
-var arr = [3, 4, 6, 1, 2];
-var small = Infinity;
-for(var i = 0; i < arr.length; i++) {
-    if(arr[i] < small) {
-        small = arr[i];
+function arrayNumber(arr, num) {
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i] == num) {
+            return i;
+        }
     }
 }
-console.log(small);
+
+var index = arrayNumber([3, 4, 6, 1, 2], 2);
+console.log(index);
