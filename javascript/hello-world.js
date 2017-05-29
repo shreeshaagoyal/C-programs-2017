93ca362c8ead6585282571751b7345be605d1549
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-05-29 11:41:44 -0700

    added timer example

--- javascript/hello-world.js
+++ javascript/hello-world.js
@@ -1,10 +1,9 @@
-function arrayNumber(arr, num) {
-    for(var i = 0; i < arr.length; i++) {
-        if(arr[i] == num) {
-            return i;
-        }
-    }
-}
+var demoFunction = function () {
+    console.log('this is a demo function.');
+};
 
-var index = arrayNumber([3, 4, 6, 1, 2], 2);
-console.log(index);
+demoFunction();
+
+setInterval(demoFunction, 1000);
+
+console.log('ok i wil do ta');
