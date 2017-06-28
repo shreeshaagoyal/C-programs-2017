ccc148c53ec9305edfadcac9d22d8efd68d2eb62
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-06-28 14:52:17 -0700

    made an object

--- java/MyFirstJavaProgram.java
+++ java/MyFirstJavaProgram.java
@@ -1,49 +1,9 @@
-class Circle implements Shape {
-    double dim;
-    public Circle(double dim) {
-        this.dim = dim;
-    }
-    public double getArea() {
-        return this.dim * this.dim * Math.PI;
-    }
-    public double getCirc() {
-        return 2 * Math.PI * this.dim;
-    }
-}
-
-class Square implements Shape {
-    double dim;
-    public Square(double dim) {
-        this.dim = dim;
-    }
-    public double getArea() {
-        return this.dim * this.dim;
-    }
-}
-
-interface Shape {
-    public double getArea();
-}
-
 public class MyFirstJavaProgram {
-    public static void main(String []args) {
-        Circle c1 = new Circle(5);
-        Square s1 = new Square(6);
-        Shape sh1;
-        
-        int inputFromUser = 101;
-        if (inputFromUser < 100) {
-            sh1 = new Circle(7);
-        } else {
-            sh1 = new Square(7);
-        }
-        
-        Circle c2 = (Circle) sh1;
-
-        c1.getCirc();
-        s1.getArea();
-        c2.getCirc();
+    public MyFirstJavaProgram(String name) {
+        System.out.println("Puppy's name is: " + name);
     }
 
-    
+    public static void main(String []args) {
+        MyFirstJavaProgram myPuppy = new MyFirstJavaProgram( "Scruffy" );
+    }
 }
