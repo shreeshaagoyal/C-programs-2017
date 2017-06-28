798bb9a5e750d469c5ad8fc194120f3c2d29a00d
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-06-28 12:13:43 -0700

    initial commit - java tutorial

---
+++ java/ListElement.java
@@ -0,0 +1,8 @@
+public class ListElement() {
+    ListElement next;
+    Object data;
+
+    public ListElement(Object data) {
+        this.data = data;
+    }
+}
--- java/Main.java
+++ java/Main.java
@@ -1,13 +1,6 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        Node n1, n2;
-        n1 = new Node(12);
-        n2 = new Node(11);
-        n1.next = n2;
-        n2.next = null;
-
-        n1.print();
-        n2.print();
-    }
+    typedef struct IntElement {
+        struct IntElement *next;
+        int data;
+    } IntElement;
 }
