a6adf93d9242dac93320be2ae1fe7a2728e4422e
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-06-28 20:38:15 -0700

    enhanced for loop

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,18 @@
 public class Main {
     public static void main(String []args) {
-        String name = "Shreeshaa";
-        boolean result = name instanceof String;
-        System.out.print("name is String? " + result);
+        int [] numbers = {10, 20, 30, 40, 50};
+
+        for(int x : numbers) {
+            System.out.print(x);
+            System.out.print(", ");
+        }
+
+        System.out.print("\n");
+        String [] names = {"Shreeshaa", "Shivanshu", "Mona", "Arun"};
+
+        for(String name : names) {
+            System.out.print(name);
+            System.out.print(", ");
+        }
     }
 }
