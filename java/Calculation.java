cf3a50d104ea57ccd41e9cd99ce93ba2b165d5c3
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-01 11:13:13 -0700

    inheritance demo pt 1

---
+++ java/Calculation.java
@@ -0,0 +1,11 @@
+public class Calculation {
+    int z;
+    public void addition(int x, int y) {
+        this.z = x + y;
+        System.out.println("Sum is: " + this.z);
+    }
+    public void subraction(int x, int y) {
+        this.z = x - y;
+        System.out.println("Difference is: " + this.z);
+    }
+}
---
+++ java/My_Calculation.java
@@ -0,0 +1,13 @@
+public class My_Calculation extends Calculation {
+    public void multiplication(int x, int y) {
+        this.z = x * y;
+        System.out.println("Product is: " + this.z);
+    }
+    public static void main(String[] args) {
+        int a = 30, b = 10;
+        My_Calculation demo = new My_Calculation();
+        demo.addition(a, b);
+        demo.subraction(a, b);
+        demo.multiplication(a, b);
+    }
+}
