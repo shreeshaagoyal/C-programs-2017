21d2eaded6b9f13f19d01fdb7eb93352de58243d
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-03 10:53:07 -0700

    made the AList

--- java/AList.java
+++ java/AList.java
@@ -1,42 +1,44 @@
 public class AList {
     int[] arr;
-    int initialCapacity;
-    int size;
-    int capacityLeft;
+    int initialCapacity = 0;
+    int size = 0;
+    int capacityLeft = 0;
     public AList(int initialCapacity) {
-        arr = new int[initialCapacity];
+        this.arr = new int[initialCapacity];
         this.initialCapacity = initialCapacity;
     }
 
     public void add(int e) {
         this.size++;
         if(capacityLeft() > 0) {
-            arr[size] = e;
+            this.arr[size-1] = e;
         } else {
-            int[] newArr = new int[2*arr.length];
-            for(int i = 0; i<arr.length; i++) {
-                newArr[i] = arr[i];
+            int[] newArr = new int[2 * this.arr.length];
+            for(int i = 0; i < this.arr.length; i++) {
+                newArr[i] = this.arr[i];
             }
-            newArr[size] = e;
+            newArr[size-1] = e;
             this.arr = newArr;
         }
     }
 
     private int capacityLeft() {
-        return arr.length - this.size;
+        return this.arr.length - this.size;
     }
 
     public void clear() {
         this.size = 0;
-        trimToSize();
     }
 
     public int get(int index) {
-        return arr[index];
+        return this.arr[index];
     }
 
     public void trimToSize() {
         int[] newArr = new int[this.size];
+        for(int i = 0; i < this.size; i++) {
+            newArr[i] = this.arr[i];
+        }
         this.arr = newArr;
     }
 }
