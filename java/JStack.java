96a2fcdb6efbfc902f5b8e9870e9f2deae8db549
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-04 12:59:03 -0700

    fixed bugs

--- java/JStack.java
+++ java/JStack.java
@@ -1,19 +1,18 @@
 public class JStack {
     List<Integer> lastElement;
     List<Integer> stack = new LinkedList<Integer>();
-    int size;
     public void push(int e) {
         this.stack.add(e);
     }
     public int pop() {
-        this.size = this.stack.size();
-        this.stack.remove(this.size-1);
-        return this.stack.get(this.size-1);
+        int result = peek();
+        this.stack.remove(this.stack.size()-1);
+        return result;
     }
     public int size() {
         return this.stack.size();
     }
     public int peek() {
-        return this.stack.get(this.size-1);
+        return this.stack.get(this.stack.size()-1);
     }
 }
