2a54a915e69ddbc984e662c996c5f3e983f2fafd
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-04 22:08:19 -0700

    fixed bug using return;

--- java/JMaps.java
+++ java/JMaps.java
@@ -1,20 +1,16 @@
 public class JMaps {
     List<Entry> entryList = new ArrayList<Entry>();
     public void set(String name, int num) {
-        boolean match = false;
         for(int i = 0; i < this.entryList.size(); i++) {
             if(name == this.entryList.get(i).name) {
                 // Replacing Entry
                 this.entryList.get(i).keyValue = num;
-                match = true;
-                break;
+                return;
             }
         }
-        if(!match) {
-            // Adding new Entry to back
-            Entry newEntry = new Entry(name, num);
-            this.entryList.add(newEntry);
-        }
+        // Adding new Entry to back
+        Entry newEntry = new Entry(name, num);
+        this.entryList.add(newEntry);
     }
     public int get(String name) {
         for(int i = 0; i < this.entryList.size(); i++) {
