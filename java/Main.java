f14a6f94df2061bcd8875d98ad71d6b6fcdfe771
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-05 17:10:36 -0700

    made stringCompare method

--- java/Main.java
+++ java/Main.java
@@ -7,4 +7,20 @@ public class Main {
         System.out.println(foo.contains("fjkdls"));
         System.out.println(foo.contains("shivanshu"));
     }
+    public int compare(String a, String b) {
+        int longerWordLength;
+        if(a.length > b.length) {
+            longerWordLength = a.length;
+        } else {
+            longerWordLength = b.length;
+        }
+        for(int i = 0; i < longerWordLength; i++) {
+            if(a.codePointAt(i) > b.codePointAt(i)) {
+                return 1;
+            } else if(a.codePointAt(i) < b.codePointAt(i)) {
+                return -1;
+            }
+        }
+        return 0;
+    }
 }
