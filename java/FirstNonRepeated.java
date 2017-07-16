82e778655ca7b5801ac50203915857684bafa6ca
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-15 23:35:29 -0700

    used character arrays. improvements coming

--- java/FirstNonRepeated.java
+++ java/FirstNonRepeated.java
@@ -2,14 +2,19 @@ import java.util.HashMap;
 public class FirstNonRepeated {
     public static char findFirstNonRepeated(String userInput) {
         HashMap str = new HashMap(userInput.length());
-        char[] charArr = userInput.toCharArray();
-        int value = 0;
+        char[] charArr = new char[userInput.length()];
+        charArr = userInput.toCharArray();
+        Integer value = new Integer(0);
+        Integer seenOnce = new Integer(1);
         for(int i = 0; i < charArr.length; i++) {
             str.put(charArr[i], value+1);
         }
         for(int i = 0; i < charArr.length; i++) {
-            if(str.get(charArr[i]) == 1) {
+            if(str.get(charArr[i]) == seenOnce) {
                 return charArr[i];
+            }
+            else {
+                throw new RuntimeException("All characters repeat");
             }
         }
     }
