cdcb8ee05c2de152a1262697113e1d475733c1e4
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-22 16:31:46 -0700

    defined my first function: timestwo

--- DrRacket_learning/abc.rkt
+++ DrRacket_learning/abc.rkt
@@ -1,10 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname abc) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
-(overlay
- (circle 23 "solid" "orange")
- (square 45 "solid" "red"))
+(define (timestwo x) (* 2 x))
 
-(star 40 "solid" "pink")
+(timestwo 3)
 
-(star-polygon 20 10 3 "outline" "cornflowerblue")
+(timestwo (timestwo 4))
