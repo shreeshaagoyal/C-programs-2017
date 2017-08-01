7d9c986bc16756ac7767d7a920d061effe8eb8ec
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-31 23:21:11 -0700

    defined method greeting using the design recipe

--- DrRacket_learning/abc.rkt
+++ DrRacket_learning/abc.rkt
@@ -1,14 +1,11 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname abc) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
-(define (timestwo x) (* 2 x))
+; greeting : string -> string
+; given a name, produces a greeting
+(define (greeting name)
+  (string-append "Best wishes to " name "."))
 
-(timestwo 3)
+(check-expect (greeting "Rufus") "Best wishes to Rufus.")
 
-(timestwo (timestwo 4))
-
-(define (red-square s) (square s "solid" "red"))
-
-(red-square 4)
-(red-square 8)
-(red-square 16)
+(greeting "Shreeshaa")
