fef55ba787700bf27784afe42b68ac96cfce18a6
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-08-01 00:02:31 -0700

    made a compound data frog

--- DrRacket_learning/abc.rkt
+++ DrRacket_learning/abc.rkt
@@ -1,15 +1,8 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname abc) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
-; weight-rejection : number -> string
-; given a weight in kilos, return a rejection string
-(define(weight-rejection weight)
-  (cond [(<= weight 52)  "Below average"]
-        [(< weight 57)  "Above average"]
-        [(>= weight 57)  "Illegal"]))
+; a frog is (make-frog string number)
+(define-struct frog (species mass))
 
-(check-expect (weight-rejection 50) "Below average")
-(check-expect (weight-rejection 52) "Below average")
-(check-expect (weight-rejection 56) "Above average")
-(check-expect (weight-rejection 57) "Illegal")
-(check-expect (weight-rejection 60) "Illegal")
+(make-frog "tree" 39)
+(make-frog "ground" 230)
