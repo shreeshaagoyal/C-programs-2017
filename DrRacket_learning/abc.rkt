7b3cab222b30dffe4fb22f8835c4b8f2e9a910d1
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-07-31 23:59:17 -0700

    implemented weight-rejection function

--- DrRacket_learning/abc.rkt
+++ DrRacket_learning/abc.rkt
@@ -1,11 +1,15 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname abc) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp")) #f)))
-; greeting : string -> string
-; given a name, produces a greeting
-(define (greeting name)
-  (string-append "Best wishes to " name "."))
+; weight-rejection : number -> string
+; given a weight in kilos, return a rejection string
+(define(weight-rejection weight)
+  (cond [(<= weight 52)  "Below average"]
+        [(< weight 57)  "Above average"]
+        [(>= weight 57)  "Illegal"]))
 
-(check-expect (greeting "Rufus") "Best wishes to Rufus.")
-
-(greeting "Shreeshaa")
+(check-expect (weight-rejection 50) "Below average")
+(check-expect (weight-rejection 52) "Below average")
+(check-expect (weight-rejection 56) "Above average")
+(check-expect (weight-rejection 57) "Illegal")
+(check-expect (weight-rejection 60) "Illegal")
