32aa8cf86e30997215be52c17b6ea114d65b7da4
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-08-03 15:08:15 -0700

    bugs to be fixed

--- java/LeetCode.java
+++ java/LeetCode.java
@@ -4,21 +4,33 @@ public class LeetCode {
     public static int lengthOfLongestSubstring(String s) {
         HashSet<Character> resultSet = new HashSet<Character>();
         HashSet<Character> tempSet = new HashSet<Character>();
-        for(int i = 0; i < s.length(); i++) {
-            if(!tempSet.contains(s.charAt(i))) {
-                tempSet.add(s.charAt(i));
+        int i;
+        for(i = 0; i < s.length(); i++) {
+            if(!resultSet.contains(s.charAt(i))) {
+                resultSet.add(s.charAt(i));
+            } else {
+                break;
+            }
+        }
+        for(int j = i; j < s.length(); j++) {
+            if(!tempSet.contains(s.charAt(j))) {
+                tempSet.add(s.charAt(j));
             } else {
                 if(tempSet.size() >= resultSet.size()) {
                     resultSet = tempSet;
                     tempSet = new HashSet<Character>();
-                    tempSet.add(s.charAt(i));
+                    tempSet.add(s.charAt(j));
                 }
             }
         }
-        return resultSet.size();
+        if(tempSet.size() > resultSet.size()) {
+            return tempSet.size();
+        } else {
+            return resultSet.size();
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("accompossssssssss"));
+        System.out.println(lengthOfLongestSubstring("aab"));
     }
 }
