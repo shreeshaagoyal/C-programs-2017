dda308640a92bad3dbf5440eda053450c33970e1
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-08-04 20:13:33 -0700

    implemented longestSubstring that checks to see what the longest palindrome in a String is. off by one error somewhere. bug to be fixed ;;!!!

--- java/LeetCode.java
+++ java/LeetCode.java
@@ -1,5 +1,7 @@
 import java.util.HashSet;
 
+import java.lang.Math;
+
 public class LeetCode {
     public static int lengthOfLongestSubstring(String s) {
         HashSet<Character> resultSet = new HashSet<Character>();
@@ -30,7 +32,41 @@ public class LeetCode {
         }
     }
 
+    public static String longestSubstring(String s) {
+        int ans = 0, maxPalindrome = 0;
+        int oddLength = 0, evenLength = 0;
+        int start = 0, end = 0;
+        for(int i = 0; i < s.length(); i++) {
+            oddLength = expandPalindrome(s, i, i);
+            evenLength = expandPalindrome(s, i, i+1);
+            maxPalindrome = Math.max(oddLength, evenLength);
+            ans = Math.max(ans, maxPalindrome);
+            if(maxPalindrome > i) {
+                if(ans%2 == 0) {
+                    start = i - (ans/2) + 1;
+                    end = i + (ans/2);
+                } else {
+                    start = i - (ans/2);
+                    end = i + (ans/2);
+                }
+                System.out.println("i: " + i);
+            }
+        }
+        System.out.println("start: " + start);
+        System.out.println("end: " + end);
+        System.out.println("ans: " + ans);
+        return s.substring(start, end);
+    }
+
+    private static int expandPalindrome(String s, int leftIndex, int rightIndex) {
+        while((leftIndex >= 0) && (rightIndex < s.length()) && (s.charAt(leftIndex) == s.charAt(rightIndex))) {
+            leftIndex--;
+            rightIndex++;
+        }
+        return (rightIndex - leftIndex - 1);
+    }
+
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("aab"));
+        System.out.println(longestSubstring("cbbd"));
     }
 }
