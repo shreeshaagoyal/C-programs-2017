71288531e8854d31a7c585e4159bc6b00dc8ca70
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-08-30 18:27:27 -0700

    removed curNode. now working with its children instead. ** LOOK AT HOW THIS VERSION IS DIFFERENT FROM THE LAST VERSION **
    the previous version did not run and threw a NullPointerException, but this one doesn't

--- java/Trie.java
+++ java/Trie.java
@@ -1,20 +1,23 @@
+import java.util.HashMap;
 public class Trie {
     private TrieNode root;
     public Trie() {
         root = new TrieNode();
     }
     public void insert(String word, int value) {
-        TrieNode curNode = root;
+        HashMap<TrieValue, TrieNode> children = root.children;
+
         for(int i = 0; i < word.length(); i++) {
             TrieValue c = new TrieValue(word.charAt(i));
-            if(!curNode.children.containsKey(c)) {
-                curNode.children.put(c, null);
+            if(!children.containsKey(c)) {
+                TrieNode t = new TrieNode();
+                children.put(c, t);
             }
-            curNode = curNode.children.get(c);
+            children = children.get(c).children;
         }
         // set leaf node
         TrieValue num = new TrieValue(value);
-        curNode.children.put(num, null);
+        children.put(num, null);
     }
     public int search(String word) {
         return 0;
