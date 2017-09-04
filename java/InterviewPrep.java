460cc5c791dc62ca74a761e393d936e10820905d
Shreeshaa Goyal <shreeshaa3@gmail.com>
2017-09-04 10:55:27 -0700

    fixed bug in findTwoElements

--- java/InterviewPrep.java
+++ java/InterviewPrep.java
@@ -8,9 +8,9 @@ import java.util.Stack;
 public class InterviewPrep {
     public static int calculateClockAngle(int hand1, int hand2) {
         int result = Math.abs(hand1 - hand2);
-        if((0 <= result) && (result < 30)) {
+        if ((0 <= result) && (result < 30)) {
             return result;
-        } else if((0 <= result) && (result < 60)) {
+        } else if ((0 <= result) && (result < 60)) {
             return 60 - result;
         } else {
             throw new IllegalArgumentException("Input must be in the range [0, 59]");
@@ -20,8 +20,8 @@ public class InterviewPrep {
     public static int removeDuplicates(int[] arr) {
         int duplicateCursor = 0;
         HashSet<Integer> uniqueElements = new HashSet<Integer>();
-        for(int scanCursor = 0; scanCursor < arr.length; scanCursor++) {
-            if(!uniqueElements.contains(arr[scanCursor])) {
+        for (int scanCursor = 0; scanCursor < arr.length; scanCursor++) {
+            if (!uniqueElements.contains(arr[scanCursor])) {
                 uniqueElements.add(arr[scanCursor]);
                 arr[duplicateCursor] = arr[scanCursor];
                 duplicateCursor++;
@@ -35,7 +35,7 @@ public class InterviewPrep {
     public static void reverseString(StringBuffer str) {
         int j = str.length() - 1;
         char temp = ' ';
-        for(int i = 0; i < str.length()/2; i++) {
+        for (int i = 0; i < str.length() / 2; i++) {
             temp = str.charAt(i);
             str.setCharAt(i, str.charAt(j));
             str.setCharAt(j, temp);
@@ -44,7 +44,7 @@ public class InterviewPrep {
     }
 
     public static boolean palindromeNumber(int num) {
-        if(num == reverseInteger(num)) {
+        if (num == reverseInteger(num)) {
             return true;
         } else {
             return false;
@@ -57,7 +57,7 @@ public class InterviewPrep {
         int result = 0;
         int modNum = 0;
         int tempResult = 0;
-        for(int i = 0; i < digits-1; i++) {
+        for (int i = 0; i < digits - 1; i++) {
             int powerOfTen = (int) Math.pow(10, j);
             modNum = num % powerOfTen;
             tempResult = (num - modNum) / powerOfTen;
@@ -65,23 +65,23 @@ public class InterviewPrep {
             j--;
             num = modNum;
         }
-        result += num * ((int) Math.pow(10, digits-1));
+        result += num * ((int) Math.pow(10, digits - 1));
         return result;
     }
 
     private static int numberOfDigits(int num) {
         int i = 1;
-        while(num > 0) {
+        while (num > 0) {
             num = num - (num % (int) Math.pow(10, i));
             i++;
         }
-        return i-1;
+        return i - 1;
     }
 
     public static int removeElement(int[] nums, int val) {
         int slowCursor = 0;
-        for(int fastCursor = 0; fastCursor < nums.length; fastCursor++) {
-            if(nums[fastCursor] == val) {
+        for (int fastCursor = 0; fastCursor < nums.length; fastCursor++) {
+            if (nums[fastCursor] == val) {
                 continue;
             } else {
                 nums[slowCursor] = nums[fastCursor];
@@ -92,28 +92,28 @@ public class InterviewPrep {
     }
 
     private static void printArray(int[] arr) {
-        for(int i = 0; i < arr.length-1; i++) {
+        for (int i = 0; i < arr.length - 1; i++) {
             System.out.print(arr[i] + ",");
         }
-        System.out.println(arr[arr.length-1]);
+        System.out.println(arr[arr.length - 1]);
     }
 
     public static boolean isValid(String s) {
-        if(s.length() == 0) {
+        if (s.length() == 0) {
             throw new IllegalArgumentException("No characters found");
         }
         Stack<Character> brackets = new Stack<Character>();
         brackets.push(s.charAt(0));
-        for(int i = 1; i < s.length(); i++) {
-            if(brackets.empty()) {
+        for (int i = 1; i < s.length(); i++) {
+            if (brackets.empty()) {
                 brackets.push(s.charAt(i));
-            } else if(oppositeParentheses(brackets.peek()) == s.charAt(i)) {
+            } else if (oppositeParentheses(brackets.peek()) == s.charAt(i)) {
                 brackets.pop();
             } else {
                 brackets.push(s.charAt(i));
             }
         }
-        if(brackets.empty()) {
+        if (brackets.empty()) {
             return true;
         } else {
             return false;
@@ -121,80 +121,54 @@ public class InterviewPrep {
     }
 
     private static char oppositeParentheses(char bracket) {
-        if(bracket == '(') {
+        if (bracket == '(') {
             return ')';
-        } else if(bracket == '[') {
+        } else if (bracket == '[') {
             return ']';
-        } else if(bracket == '{') {
+        } else if (bracket == '{') {
             return '}';
         } else {
             return '\u0000';
         }
     }
 
-/*
-    public static int mostFrequent(int[] arr) {
-        if(arr.length == 0) {
-            throw new Exception("Empty array");
-        }
-
-        HashMap<Integer, Integer> frequencyNums = new HashMap<Integer, Integer>();
-        for(int i = 0; i < arr.length; i++) {
-            if(frequencyNums.containsKey(arr[i])) {
-                frequencyNums.put(arr[i], (frequencyNums.get(arr[i]))+1);
-            } else {
-                frequencyNums.put(arr[i], 1);
-            }
-        }
-        int frequency = 0, result = 0;
-        Integer[] keyArr = frequencyNums.keySet().toArray();
-        for(int i = 0; i < keyArr.length; i++) {
-            if(frequencyNums.get(keyArr[i]) > frequency) {
-                frequency = frequencyNums.get(keyArr[i]);
-                result = keyArr[i];
-            }
-        }
-        return result;
-    }
-*/
-
     public static String leastCommonWord(String sentence) {
 
         HashMap<String, Integer> wordFrequency = new HashMap<String, Integer>();
         String[] wordsArr = stringToWords(sentence);
-        
-        if(wordsArr.length == 0) {
+
+        if (wordsArr.length == 0) {
             throw new IllegalArgumentException("No words found");
         }
-        
-        for(int i = 0; i < wordsArr.length; i++) {
-            if(wordFrequency.containsKey(wordsArr[i])) {
+
+        for (int i = 0; i < wordsArr.length; i++) {
+            if (wordFrequency.containsKey(wordsArr[i])) {
                 wordFrequency.put(wordsArr[i], wordFrequency.get(wordsArr[i]) + 1);
             } else {
                 wordFrequency.put(wordsArr[i], 1);
             }
         }
-        
+
         double frequency = Double.POSITIVE_INFINITY;
         String result = wordsArr[0];
-        for(int i = 0; i < wordsArr.length; i++) {
-            if(wordFrequency.get(wordsArr[i]) < frequency) {
+        for (int i = 0; i < wordsArr.length; i++) {
+            if (wordFrequency.get(wordsArr[i]) < frequency) {
                 frequency = wordFrequency.get(wordsArr[i]);
                 result = wordsArr[i];
             }
         }
-        
+
         return result;
     }
 
     private static String[] stringToWords(String sentence) {
         ArrayList<String> words = new ArrayList<String>();
-        for(int i = 0; i < sentence.length(); i++) {
-            if(sentence.charAt(i) == ' ') {
+        for (int i = 0; i < sentence.length(); i++) {
+            if (sentence.charAt(i) == ' ') {
                 continue;
             }
             StringBuffer word = new StringBuffer();
-            while((sentence.charAt(i) != ' ') && (i < sentence.length()-1)) {
+            while ((sentence.charAt(i) != ' ') && (i < sentence.length() - 1)) {
                 word.append(sentence.charAt(i));
                 i++;
             }
@@ -202,7 +176,7 @@ public class InterviewPrep {
         }
 
         String[] result = new String[words.size()];
-        for(int i = 0; i < result.length; i++) {
+        for (int i = 0; i < result.length; i++) {
             result[i] = words.get(i);
         }
 
@@ -210,37 +184,37 @@ public class InterviewPrep {
     }
 
     public static int searchInsert(int[] nums, int target) {
-        return searchInsert(nums, 0, nums.length-1, target);
+        return searchInsert(nums, 0, nums.length - 1, target);
     }
 
     private static int searchInsert(int[] nums, int start, int end, int target) {
         // base case
-        if(end - start == 0) {
-            if(target < nums[start]) {
+        if (end - start == 0) {
+            if (target < nums[start]) {
                 return start;
             } else {
-                return start+1;
+                return start + 1;
             }
         }
-        
-        int mid = (end - start)/2;
-        if(target == nums[mid]) {
+
+        int mid = (end - start) / 2;
+        if (target == nums[mid]) {
             return mid;
-        } else if(target > nums[mid]) {
+        } else if (target > nums[mid]) {
             return searchInsert(nums, mid + 1, end, target);
         } else {
             return searchInsert(nums, start, mid - 1, target);
         }
     }
 
-    public static int mostFrequent (int[] arr) {
-        if(arr.length == 0) {
+    public static int mostFrequent(int[] arr) {
+        if (arr.length == 0) {
             throw new IllegalArgumentException("Empty array");
         }
 
         HashMap<Integer, Integer> frequencyNums = new HashMap<Integer, Integer>();
-        for(int i = 0; i < arr.length; i++) {
-            if(frequencyNums.containsKey(arr[i])) {
+        for (int i = 0; i < arr.length; i++) {
+            if (frequencyNums.containsKey(arr[i])) {
                 frequencyNums.put(arr[i], frequencyNums.get(arr[i]) + 1);
             } else {
                 frequencyNums.put(arr[i], 1);
@@ -249,8 +223,8 @@ public class InterviewPrep {
 
         int maxNum = arr[0];
         double maxFrequencySoFar = Double.NEGATIVE_INFINITY;
-        for(int i = 0; i < arr.length; i++) {
-            if(frequencyNums.get(arr[i]) > maxFrequencySoFar) {
+        for (int i = 0; i < arr.length; i++) {
+            if (frequencyNums.get(arr[i]) > maxFrequencySoFar) {
                 maxFrequencySoFar = frequencyNums.get(arr[i]);
                 maxNum = arr[i];
             }
@@ -259,8 +233,49 @@ public class InterviewPrep {
         return maxNum;
     }
 
+    /** 
+    Given a sorted array like the following:
+    [2, 5, 7, 8, 9, 10, 22]
+    
+    And a target value like the following:
+    15
+    
+    Find 2 elements in the array which add up to that number
+     */
+
+    public static Tuple findTwoElements(int[] arr, int target) {
+        return findTwoElements(arr, target, 0, arr.length-1);
+    }
+
+    private static Tuple findTwoElements(int[] arr, int target, int leftIndex, int rightIndex) {
+        int sum = arr[leftIndex] + arr[rightIndex];
+        // base case
+        if(rightIndex < 0) {
+            return null;
+        }
+        if(leftIndex > arr.length-1) {
+            return null;
+        }
+        if(rightIndex - leftIndex == 0) {
+            return null;
+        }
+        
+        if(sum == target) {
+            Tuple result = new Tuple();
+            result.first = arr[leftIndex];
+            result.second = arr[rightIndex];
+            return result;
+        } else if(sum > target) {
+            return findTwoElements(arr, target, leftIndex, rightIndex-1);
+        } else {
+            return findTwoElements(arr, target, leftIndex+1, rightIndex);
+        }
+    }
+
     public static void main(String[] args) {
-        int[] arr = {};
-        System.out.println(mostFrequent(arr));
+        int[] arr = {2, 5, 6, 8, 9, 10, 12};
+        Tuple findTwoElementsResult = findTwoElements(arr, 12);
+        System.out.println("findTwoElementsResult.first = " + findTwoElementsResult.first);
+        System.out.println("findTwoElementsResult.second = " + findTwoElementsResult.second);
     }
 }
