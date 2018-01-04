bee88d38ac8438192ce558e7bf2911ec163065bc
Shreeshaa Goyal <shreeshaa3@gmail.com>
2018-01-04 00:22:45 -0800

    started implementing MaxHeap

---
+++ java/MyMaxHeap.java
@@ -0,0 +1,32 @@
+public class MyMaxHeap {
+    BSTNode root;
+
+    public MyMaxHeap() {
+        this.root = null;
+    }
+
+    public MyMaxHeap(int n) {
+        this.root.value = n;
+    }
+
+    public int getMax() {
+        return this.root.value;
+    }
+
+    public int getMin() {
+        
+    }
+
+    // prints heap in chronolical order
+    public void print() {
+
+    }
+
+    public void add() {
+
+    }
+
+    public void remove() {
+
+    }
+}
